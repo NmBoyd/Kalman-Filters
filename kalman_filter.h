7741aad1cbf903119ee3a6dbb36048f2c0f4e636
Nathan Boyd <nathan.boyd.481@my.csun.edu>
2019-04-28 00:11:00 -0700

    Starting

---
+++ kalman_filter.h
@@ -0,0 +1,9 @@
+/**
+ * Kalman Filter based on the introductory paper:
+ * http://www.cs.unc.edu/~welch/media/pdf/kalman_intro.pdf
+ * 
+ * @author Nathan Boyd
+ * @ 2019.4.28
+ */
+ 
+ #include <Eigen/Dense>
